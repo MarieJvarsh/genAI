65c5f44ae26460ed818f52c0756c5c709e5dfc3b
IosebOne <45330280+iosebi2899@users.noreply.github.com>
2023-01-27 14:44:57 +0400

    feat: added loadidng scree

--- script/index.js
+++ script/index.js
@@ -3,11 +3,47 @@ const industry = document.getElementById("industry");
 const goals = document.getElementById("goals");
 const targetMarket = document.getElementById("target-market");
 const res = document.getElementById("res");
+const pacMan = document.getElementById("pac-man");
 
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
+  if (
+    industry.value === "" ||
+    goals.value === "" ||
+    targetMarket.value === ""
+  ) {
+    alert("Please fill all the fields");
+    return;
+  }
+
+  pacMan.style.display = "block";
+  setTimeout(() => {
+    pacMan.style.opacity = "1";
+    pacMan.style.marginBlock = "2em";
+    pacMan.style.marginBottom = "4em";
+  }, 30);
+  submitButton.disabled = true;
+  submitButton.style.cursor = "not-allowed";
+  submitButton.value = "Generating...";
+  res.style.paddingBlock = "0px";
+  res.style.margin = "0px";
+  res.style.marginTop = "0px";
+  res.style.height = "0px";
+  setTimeout(() => {
+    res.innerHTML = "";
+  }, 30);
+
+  submitButton.style.backgroundColor = "grey";
   analyzeGpt(industry.value, goals.value, targetMarket.value).then((data) => {
-    res.style.padding = "30px";
+    submitButton.disabled = false;
+    submitButton.style.cursor = "pointer";
+    submitButton.style.backgroundColor = "#0088a0";
+    submitButton.value = "Generate Now";
+    pacMan.style.display = "none";
+    pacMan.style.opacity = "0";
+    pacMan.style.marginBlock = "0em";
+    res.style.paddingBlock = "30px";
+    res.style.height = "auto";
     res.style.margin = "30px";
     res.innerHTML = data;
   });
@@ -16,13 +52,12 @@ submitButton.addEventListener("click", (e) => {
 const analyzeGpt = async (industry, goals, targetMarket) => {
   // make post request to gpt3 api
   const prompt = `Write a business plan for a ${industry} industry that aims to ${goals} and target ${targetMarket} market`;
-  console.log(prompt);
   const response = await fetch("https://api.openai.com/v1/completions", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       Authorization:
-        "Bearer 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",
+        "Bearer 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",
     },
     body: JSON.stringify({
       model: "text-davinci-003",
@@ -33,13 +68,6 @@ const analyzeGpt = async (industry, goals, targetMarket) => {
   });
 
   const data = await response.json();
-  console.log(data.choices[0]);
+  console.log(data);
   return data.choices[0].text.replace(/\n/g, "<br />");
 };
-
- 
-// document.getElementById("submit-button").addEventListener("click", function() {
-  // document.getElementById("loading").style.display = "block";
- 
-// });
-// document.getElementById("loading").style.display = "none"; 
